4ece56e7c59ddc29deb53e3031b54e885b36f19c
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-05 17:29:12 +0300

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home_page#index"
+  resources :users
 end
