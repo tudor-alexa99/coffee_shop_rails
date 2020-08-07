d2ddf12093fd78e89f671f6e1e9743b285f89c3d
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-07 15:26:08 +0300

    add dishes form

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,16 @@
 class HomePageController < ApplicationController
   def index
   end
+
+  def create
+    debugger
+    @dish = Dish.new(dish_params)
+    @dish.image.attach(params[:dish][:image])
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:title, :category, :price, :image)
+  end
 end
