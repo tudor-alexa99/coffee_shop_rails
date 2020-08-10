abd9e139f1000e218f8a2e111b33956676a4d2e4
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-10 15:00:48 +0300

    Add tests for controllers

---
+++ spec/requests/session_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :request do
+  before do
+    post users_url, params: {
+        user: {name: "Test", email: "test@user.com", password: "password", password_confirmation: "password"}
+    }
+    delete logout_url
+  end
+
+  it 'Get login page' do
+    get login_path
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'Log in successfully' do
+    post login_path, params: {session: {email: 'test@user.com', password: 'password' }}
+    expect(response).to have_http_status(:redirect)
+  end
+
+  it 'Login with wrong information' do
+    post login_path, params: { session: { email: '', password: '' } }
+    expect(response).to have_http_status(:success)
+    expect(flash.empty?).to be false
+  end
+end
