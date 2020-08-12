4dafd9721bb540d3273b9c27af25c0012bd1054d
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-12 15:25:25 +0300

    Add a single cart controller

--- db/migrate/20200812121336_create_cart_items.rb
+++ db/migrate/20200812121336_create_cart_items.rb
@@ -1,7 +1,7 @@
 class CreateCartItems < ActiveRecord::Migration[6.0]
   def change
     create_table :cart_items do |t|
-      t.integer, :cart_id
+      t.integer :cart_id
       t.integer :dish_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_111347) do
+ActiveRecord::Schema.define(version: 2020_08_12_121336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2020_08_12_111347) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "dish_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "dishes", force: :cascade do |t|
