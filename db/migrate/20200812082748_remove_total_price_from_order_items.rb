8f16a6ee6b91ba6d153fc0e4be3e3980bdc60805
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-14 11:13:57 +0300

    Remove order migrations

--- db/migrate/20200812082748_remove_total_price_from_order_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTotalPriceFromOrderItems < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :order_items, :total_price, :float
-  end
-end
--- db/migrate/20200812082906_remove_total_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTotalFromOrders < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :orders, :total, :float
-  end
-end
