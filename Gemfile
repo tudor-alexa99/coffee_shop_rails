9df9d165cb70d6307b68b688cb36afb5b0f8198b
Tudor Alexa <tudor.alexa99@gmail.com>
2020-08-20 21:07:46 +0300

    Add qr code

--- Gemfile
+++ Gemfile
@@ -22,6 +22,10 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'barby', '~> 0.6.8'
+
+gem 'rqrcode', '~> 1.1', '>= 1.1.2'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'bcrypt'
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,19 @@
+require 'barby'
+require 'barby/barcode'
+require 'barby/barcode/qr_code'
+require 'barby/outputter/png_outputter'
+
 module OrdersHelper
   def copy_cart_items(cart_items, order_id)
     cart_items.each do |ci|
       OrderItem.create(order_id: order_id, dish_id: ci.dish_id, quantity: ci.quantity)
     end
+  end
+
+  def generate_qr(text)
+
+    barcode = Barby::QrCode.new(text, level: :q, size: 5)
+    base64_output = Base64.encode64(barcode.to_png({xdim: 5}))
+    "data:image/png;base64,#{base64_output}"
   end
 end
